5abfb9f811f7b0a047187c069819c28555fec3bf
chuan1983 <chuan1983126@gmail.com>
2016-09-26 06:02:44 +0800

    v3

--- app/src/main/java/com/example/user/myapp/GameStopActivity.java
+++ app/src/main/java/com/example/user/myapp/GameStopActivity.java
@@ -1,15 +1,26 @@
 package com.example.user.myapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class GameStopActivity extends AppCompatActivity {
 
     private String score = "";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +32,13 @@ public class GameStopActivity extends AppCompatActivity {
         initStopButton();
         initreturnButton();
         initText();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
     private void initStopButton() {
-        Button button = (Button)findViewById(R.id.button_stop);
+        Button button = (Button) findViewById(R.id.button_stop);
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -37,8 +52,9 @@ public class GameStopActivity extends AppCompatActivity {
             }
         });
     }
+
     private void initreturnButton() {
-        Button button = (Button)findViewById(R.id.button_return);
+        Button button = (Button) findViewById(R.id.button_return);
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -49,13 +65,49 @@ public class GameStopActivity extends AppCompatActivity {
         });
     }
 
-    private void jump(){
-        Intent intent = new Intent(this,MainActivity.class);
+    private void jump() {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
-    private void initText(){
-        TextView text = (TextView)findViewById(R.id.textView_score);
+    private void initText() {
+        TextView text = (TextView) findViewById(R.id.textView_score);
         text.setText(score);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("GameStop Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
